1ea7e1ae6df9f3d52ca7f8d7c5ac68938a013bc1
pbellgitup <53939046+pbellgitup@users.noreply.github.com>
2024-11-29 12:44:56 -0600

    Update streamlit_app.py
    Testing to show dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,14 +15,13 @@ st.write(
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("zenas_athleisure_db.products.catalog_for_website").select(col('color_or_style'), col('price'), col('size_list'), col('upsell_product_desc'), col('file_url'))
+#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+my_dataframe = session.table("zenas_athleisure_db.products.catalog_for_website").select(col('COLOR_OR_STYLE'), col('PRICE'), col('SIZE_LIST'), col('UPSELL_PRODUCT_DESC'), col('FILE_URL'))
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
-pd_df = my_dataframe.to_pandas()
-
+#pd_df = my_dataframe.to_pandas()
 
 st.dataframe(data=my_dataframe, use_container_width=True)
-
 st.stop()
 
 
