f39f1aec736d35791397bac61878e9e2d8f1953f
pbellgitup <53939046+pbellgitup@users.noreply.github.com>
2024-11-29 12:43:12 -0600

    Update streamlit_app.py
    Updated to use Streamlit and Pandas.  Stopping after displaying dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col
+import requests
+import pandas as pd
 
 
 # Write directly to the app
@@ -11,14 +11,19 @@ st.write(
     """
 )
 
+#  Establish connection for Streamlit
+cnx = st.connection("snowflake")
+session = cnx.session()
 
-#name_on_order = st.text_input("Name on Smoothie:")
-#st.write("The name on your Smoothie will be: ", name_on_order)
-
-
-session = get_active_session()
 my_dataframe = session.table("zenas_athleisure_db.products.catalog_for_website").select(col('color_or_style'), col('price'), col('size_list'), col('upsell_product_desc'), col('file_url'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+
+# Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
+pd_df = my_dataframe.to_pandas()
+
+
+st.dataframe(data=my_dataframe, use_container_width=True)
+
+st.stop()
 
 
 selected_color = st.selectbox(
